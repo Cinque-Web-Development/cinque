511fa85fc9f1c198ee64065dcb8d4217c3927b60
Kathleen Stickel <kstick9210@outlook.com>
2020-12-10 19:30:58 -0600

    render gaming news tweets

--- src/services/twitter-api.js
+++ src/services/twitter-api.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 const CORS = "https://kstick-cors-anywhere.herokuapp.com/";
+const token = process.env.REACT_APP_TWITTER_BEARER_TOKEN
 const options = {
-    headers: { "Bearer": process.env.REACT_APP_TWITTER_BEARER_TOKEN }
-}
+    headers: { "Authorization": `Bearer ${token}` }
+};
 
 export function fetchTweets() {
     return axios.get(`${CORS}https://api.twitter.com/2/tweets/search/recent?query=from:OneMinGameNews&tweet.fields=created_at,attachments&expansions=author_id&media.fields=media_key&user.fields=description`, options)
