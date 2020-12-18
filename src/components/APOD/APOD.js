6695ac96d33dcbf49f536065b961e03d002f3a9f
Kathleen Stickel <kstick9210@outlook.com>
2020-12-18 12:39:23 -0500

    addl styling

--- src/components/APOD/APOD.js
+++ src/components/APOD/APOD.js
@@ -4,7 +4,7 @@ import './APOD.css'
 export default function APOD({title, picture, description}) {
     return (
         <div className="apod">
-            <h2 className="apod-title">{title}</h2>
+            <h1 className="apod-title">{title}</h1>
             <img alt="" src={picture} className="apod-pic"></img>
             <p className="apod-descrip">{description}</p>
             <p className="apod-descrip">NASA Astronomy Picture of the Day</p>
