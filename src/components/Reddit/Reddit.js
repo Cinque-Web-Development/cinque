342e1c79f35ffdc6f3a18eb5ceaca7873c60590c
Samuel Trahan <samtrahan91@gmail.com>
2020-12-20 19:56:27 -0600

    got the reddit api working

--- src/components/Reddit/Reddit.js
+++ src/components/Reddit/Reddit.js
@@ -1,32 +1,41 @@
 import React, {useState, useEffect} from 'react';
 import './Reddit.css';
-import axios from 'axios';
 import Article from './Article';
 
 export default function Reddit() {
-    const [articles, setArticle] = useState({});
+    const [articles, setArticle] = useState();
+    const [links, setLinks] = useState('');
+    const [subReddit, setSubReddit] = useState('')
    
 
 
     useEffect(() => {
-        axios.get(`https://www.reddit.com/r/reactjs.json`)
+        fetch(`https://www.reddit.com/r/webdev.json?&limit=10`)
         .then(res => {
             if(res.status !== 200) {
                 console.log("Error")
                 return;
             }
-            console.log(res.data.data.children)
-            setArticle(res.data.data.children)
+            res.json().then((response) => {
+                response.data.children.map((item) => {
+                    console.log(item.data)
+                    setArticle(item.data.title)
+                    setLinks(item.data.permalink)
+                    setSubReddit(item.data.subreddit)
+                })
+            })
         })
     }, [])
+
+    console.log(articles)
+    console.log(links)
+    console.log(subReddit)
     
 
     console.log(articles)
     return (
-        <div>
-           {articles.map((article, index) => 
-               <Article key={index} article={article.data} />
-           )}
+        <div> 
+            <Article articles={articles} links={links} subReddit={subReddit} />      
         </div>
     )
 }
