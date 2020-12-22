5019768fc86a7382add4c4eeaa532209efc5df3c
Samuel Trahan <samtrahan91@gmail.com>
2020-12-22 09:30:31 -0600

    reddit comp working and displaying

--- src/components/Reddit/Article.js
+++ src/components/Reddit/Article.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Articles.css'
 
-export default function Article({articles, links, subReddit}) {
+export default function Article({article, link, subReddit}) {
     return (
         <div className="reddit-section">
-            <a className="reddit" href={"https://reddit.com" + links} rel="noreferrer" target="_blank">
-               <h3>{articles}</h3>
+            <a className="reddit" href={"https://reddit.com" + link} rel="noreferrer" target="_blank">
+               <h3>{article}</h3>
               <h5>{subReddit}</h5>
             </a>
         </div>
