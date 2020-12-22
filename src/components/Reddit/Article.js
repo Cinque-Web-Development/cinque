afcc765eace0e2f16974bfc15b45b53037311377
Kathleen Stickel <kstick9210@outlook.com>
2020-12-22 16:25:59 -0500

    lots of styling

--- src/components/Reddit/Article.js
+++ src/components/Reddit/Article.js
@@ -3,10 +3,9 @@ import './Articles.css'
 
 export default function Article({article, link, subReddit}) {
     return (
-        <div className="reddit-section">
+        <div className="article">
             <a className="reddit" href={"https://reddit.com" + link} rel="noreferrer" target="_blank">
-               <h3>{article}</h3>
-              <h5>{subReddit}</h5>
+                <h2 className="reddit-title">{article} - <span className="subreddit">{subReddit}</span></h2>
             </a>
         </div>
     )
--- src/components/Reddit/Reddit.js
+++ src/components/Reddit/Reddit.js
@@ -3,12 +3,16 @@ import './Reddit.css';
 import Article from './Article';
 import Loader from '../Loader/Loader';
 
+import jsIcon from "../../Assets/subred_js.png";
+import reactIcon from "../../Assets/subred_react.png";
+import techIcon from "../../Assets/subred_tech.png";
+import webdevIcon from "../../Assets/subred_webdev.png";
+import otherIcon from "../../Assets/subred_other.webp";
+
 export default function Reddit() {
     const [articles, setArticle] = useState();
     const [subReddits, setSubReddits] = useState("webdev")
     
-   
-
 
     useEffect(() => {
         fetch(`https://www.reddit.com/r/${subReddits}.json?&limit=7`)
@@ -26,9 +30,11 @@ export default function Reddit() {
     
 
     const getArticles = articles ? articles.map((article) => 
-        <Article  article={article.data.title} link={article.data.permalink} subReddit={article.data.subReddit} />      
+        <Article  article={article.data.title} link={article.data.permalink} subReddit={article.data.subreddit} key={article.data.title} />      
     ) : <Loader />
     
+
+    let subredIcon = subReddits === "javascript" ? jsIcon : subReddits === "reactjs" ? reactIcon : subReddits === "technology" ? techIcon : subReddits === "webdev" ? webdevIcon : otherIcon;
 
     const handleChange = (e) => {
         setSubReddits(e.target.value)
@@ -39,24 +45,24 @@ export default function Reddit() {
     return (
         
           <div className="reddit-section">
-              <h1 className="reddit-logo"><i className="reddit-logo fab fa-reddit-alien fa-2x"></i>Subreddits</h1>
+              <h1 className="reddit-logo"><i className="reddit-logo fab fa-reddit-alien fa-2x"></i>subreddits</h1>
               <form >
-            <select 
-            onChange={handleChange}
-            name="dropdown"
-            style={{display: "block"}}
-            value={subReddits}
-            className="doprdown-form"
-            >
-            <option value=""></option>
-            <option value="webdev">Web Dev</option>
-            <option value="reactjs">React JS</option>
-            <option value="softwareengineering">Software Engineering</option>
-            <option value="javascript">JavaScript</option>
-            <option value="technology">Technology</option>
-           
-            </select>
+                <select 
+                    onChange={handleChange}
+                    name="dropdown"
+                    style={{display: "block"}}
+                    value={subReddits}
+                    className="dropdown-form"
+                >
+                    <option className="subreddit-option" value=""></option>
+                    <option className="subreddit-option" value="webdev">Web Dev</option>
+                    <option className="subreddit-option" value="reactjs">React JS</option>
+                    <option className="subreddit-option" value="softwareengineering">Software Engineering</option>
+                    <option className="subreddit-option" value="javascript">JavaScript</option>
+                    <option className="subreddit-option" value="technology">Technology</option>
+                </select>
               </form>
+                <img src={subredIcon} alt="subreddit icon" className="subreddit-icon"></img>
               {getArticles}
           </div>  
     )
