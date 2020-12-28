4f342ca1934d965a31bc8d93fd6de3cf0ebc6eed
Kathleen Stickel <kstick9210@outlook.com>
2020-12-28 16:02:27 -0500

    add logo select logic

--- src/components/Sports/Sports.js
+++ src/components/Sports/Sports.js
@@ -12,23 +12,37 @@ import pga from "../../Assets/pga.png";
 
 export default function Sports() {
     const [games, setGames] = useState();
+    const [logoElements, setLogoElements] = useState([])
 
-    async function getGames(id) {
+    async function getGames(e, id) {
         const gamesResults = await fetchSports(id);
         setGames(gamesResults.data.events.slice(0,10));
+        if (e) {
+            for (let i = 0; i < logoElements.length; i++) {
+                if (logoElements[i].classList.contains("current")) {
+                    logoElements[i].classList.remove("current")
+            }}
+            e.target.classList.add("current");
+        }
+    }
+
+    function getLogoEls() {
+        const logos = document.getElementsByClassName("logo");
+        setLogoElements(logos);
     }
 
     useEffect(() => {
-        getGames(4387)
+        getGames("", 4387);
+        getLogoEls();
     }, [])
 
     return (
         <div className="sports-comp">
             <nav className="sports-nav">
-                <img className="nba logo" src={nba} alt="NBA Logo" onClick={(e) => getGames(4387)}/>
-                <img className="nfl logo" src={nfl} alt="NFL Logo" onClick={(e) => getGames(4391)}/>
-                <img className="mlb logo" src={mlb} alt="MLB Logo" onClick={(e) => getGames(4424)}/>
-                <img className="pga logo" src={pga} alt="PGA Tour Logo" onClick={(e) => getGames(4425)}/>
+                <img className="nba logo current" src={nba} alt="NBA Logo" onClick={(e) => getGames(e, 4387)}/>
+                <img className="nfl logo" src={nfl} alt="NFL Logo" onClick={(e) => getGames(e, 4391)}/>
+                <img className="mlb logo" src={mlb} alt="MLB Logo" onClick={(e) => getGames(e, 4424)}/>
+                <img className="pga logo" src={pga} alt="PGA Tour Logo" onClick={(e) => getGames(e, 4425)}/>
             </nav>
             {games ? (
                 <div>
